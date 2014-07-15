3f155be42617d2b83d982a05c4d9eb8d3376dc5d
Naoki Ikeno <nao@kyo-to.net>
2014-07-15 15:41:48 +0900

    add index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include 'config.php';
+include 'class.php';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>CRM</title>
+  <link rel="stylesheet" href="default.css" name="display">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">  
+</head>
+<body>
+  
+
+<?php
+calender();
+?>
+
+</body>
+</html>
