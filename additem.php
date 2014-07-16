dd59c7f59698dccb95d0d17f83ca62b19916e166
Naoki Ikeno <nao@kyo-to.net>
2014-07-16 20:44:01 +0900

    Add the additem.php.

---
+++ additem.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>講座予約</title>
+  <link rel="stylesheet" href="default.css" name="display">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">  
+</head>
+<body>
+
+</body>
