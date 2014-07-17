40fbaa3349f1a49f8e05097c70763dbaf4936b81
Naoki Ikeno <nao@kyo-to.net>
2014-07-17 15:56:58 +0900

    Remove default.css line.

--- index.php
+++ index.php
@@ -14,7 +14,6 @@ if( isset( $_GET['month'] )){
 <head>
   <meta charset="utf-8">
   <title>講座予約</title>
-  <link rel="stylesheet" href="default.css" name="display">
   <meta name="viewport" content="width=device-width">
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/style.css">  
