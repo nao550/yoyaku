a49eb1ff1d608505b6abd4730ed5eebb611d5020
Naoki Ikeno <nao@kyo-to.net>
2014-07-17 15:36:37 +0900

    Implementation of additional code.

--- additem.php
+++ additem.php
@@ -1,4 +1,9 @@
 <?php
+include 'config.php';
+
+if( isset( $_POST['mode'] )){
+   add_yoyaku( $_POST );
+}
 
 if(! isset( $_GET['date'] )){
     header( 'Location: index.php' );
@@ -6,8 +11,8 @@ if(! isset( $_GET['date'] )){
     $date = $_GET['date'];
     $class = $_GET['class'];
 }
-
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -22,11 +27,12 @@ if(! isset( $_GET['date'] )){
 
   <h1 id="pagetitle">受講予約</h1>
 
-  <!-- <?php   echo $_GET['date'] . " " . $date . " " . $class ; ?> -->
+  <!-- <?php echo "sql:" . $sql; ?> -->
 
   <form action="additem.php" method="POST" id="yoyaku_admit">
     <label>日付：</label>
-    <input type="text" name="date" size="15" readonly value="<?php echo date("Y年m月d日",$date) ?>" />
+    <input type="text" name="date_p" size="15" readonly value="<?php echo date("Y年m月d日",$date) ?>" />
+    <input type="hidden" name="date" size="15" readonly value="<?php echo date("Y-m-d",$date) ?>" />
     <input type="text" name="class" size="2" readonly value="<?php echo $class ?>" />
     <label>時限目</label>
     <br />
@@ -36,13 +42,32 @@ if(! isset( $_GET['date'] )){
     <label>氏名：</label>
     <input type="text" name="studentnm" />
     <br />
+    <input type="hidden" name="mode" value="add">
     <input type="submit" value="予約">
     <a href="index.php"><input type="button" value="戻る" /></a>
   </form>
-    
-
-
-    
 
 </body>
 </html>
+
+<?php
+
+function add_yoyaku( $POST ){
+
+   global $DBSV, $DBUSER , $DBPASS , $DBNM;
+
+   $date = $POST['date'];
+   $class = $POST['class'];
+   $studentid = $POST['studentid'];
+   $studentnm = $POST['studentnm'];
+   $regdate = date("Y-m-d H:i:s");
+
+   $sql = "insert into yoyaku ( date, class, studentid, studentnm, regdate ) value ( '$date', '$class', '$studentid', '$studentnm', '$regdate' )";
+
+   $link = mysqli_connect( $DBSV, $DBUSER , $DBPASS , $DBNM ) or die(mysqli_connect_error()); 
+    
+   mysqli_query( $link, $sql) or die(mysqli_error( $link ));
+
+}
+
+?>
