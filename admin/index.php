1b25eef377045349fa480b041bd652d2df99571a
Naoki Ikeno <nao@kyo-to.net>
2014-07-23 16:18:57 +0900

    Add the admin/index.php.

---
+++ admin/index.php
@@ -0,0 +1,24 @@
+<?php
+include '../config.php';
+include '../class.php';
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>管理画面</title>
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/style.css">  
+</head>
+<body>
+<h1>管理者ログイン</h1>
+
+<?php
+    ScanYoyaku();
+?>
+
+</body>
+</html>
+
