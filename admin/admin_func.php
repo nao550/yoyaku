98afc79e7ac802bd77c28a733309e28b842f6f13
Naoki Ikeno <nao@kyo-to.net>
2014-07-28 17:07:28 +0900

    Add administration manual link.

--- admin/admin_func.php
+++ admin/admin_func.php
@@ -9,6 +9,7 @@ print <<< EOF
     <li><a href="listview.php">学生一覧表示</a></li>
     <li><a href="checkday.php">日付設定</a></li>
     <li><a href="index.php?mode=destroy">ログオフ</a></li>
+    <li><a href="../docs/admin/_build/html/index.html">マニュアル</a></li>
   </ul>
 </div>
 EOF;
