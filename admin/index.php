1eb57978a8e8e8053b1211c5e0a1a872e3707884
Naoki Ikeno <nao@kyo-to.net>
2014-07-29 14:11:00 +0900

    Add the bootstrap style.

--- admin/index.php
+++ admin/index.php
@@ -38,21 +38,55 @@ if( isset( $_POST['user'] ) and isset( $_POST['password'] )){
 <head>
   <meta charset="utf-8">
   <title>管理画面</title>
-  <meta name="viewport" content="width=device-width">
-  <link rel="stylesheet" href="../css/reset.css">
-  <link rel="stylesheet" href="../css/style.css">  
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">  
+
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-  <h1>管理者ログイン</h1>
 
-
-  <form action="index.php" method="POST" id="loginform">
-    <label>ログイン名：</label>
-    <input type="text" id="user" name="user" width='20' /><br />
-    <label>パスワード：</label>
-    <input type="text" id="password" name="password" width='20' /><br />
-    <input type="submit" value="ログイン" />
-    <input type="reset" value="reset" />
+  <div class="container">
+  <form action="index.php" method="POST" id="loginform" class="form-signin" role="form">
+    <h2 class="form-signin-heading">管理者ログイン</h2>
+    <input type="text" id="user" name="user" class="form-control" placeholder="ログインID" required autofocus />
+    <input type="password" id="password" name="password" class="form-control" placeholder="パスワード" required />
+    <div class="checkbox">
+      <label>
+	<input type="checkbox" value="remember-me"> パスワードを記憶する</label>
+    </div>
+    <button class="btn btn-primary btn-block" type="submit">ログイン</button>
   </form>
+  </div>
 </body>
 </html>
